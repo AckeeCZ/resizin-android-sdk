8346b7edf5d2dfcb035e5e96337a2e2ea4cc1ba4
David Bilik <david.bilik@ackee.cz>
2019-07-24 15:14:44 +0200

    Migrate app to android X, showcase usage of sdk

--- app/src/main/java/cz/ackee/resizin/MainActivity.java
+++ app/src/main/java/cz/ackee/resizin/MainActivity.java
@@ -1,13 +1,39 @@
 package cz.ackee.resizin;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewTreeObserver;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    Resizin resizin = new Resizin("ackee");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ImageView img = findViewById(R.id.image);
+        img.getViewTreeObserver().addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
+            @Override
+            public boolean onPreDraw() {
+                img.getViewTreeObserver().removeOnPreDrawListener(this);
+                String url = resizin.url()
+                    .crop(Crop.FACE)
+                    .width(img.getWidth())
+                    .height(img.getHeight())
+                    .generate("player");
+
+                Picasso.get()
+                    .load(url)
+                    .into(img);
+
+                return false;
+            }
+        });
     }
 }
